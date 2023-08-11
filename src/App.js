b6c4c321e0e899fb6ff79c33231841e2f48535a8
Anushka Agrawal <agrawalanushka512@gmail.com>
2023-08-11 22:23:40 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from "react";
 import Home from "./Pages/Home/Home.js"
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom" ;
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import Contact from "./Pages/Contact/Contact.js";
 import ErrorPage from "./Pages/Error/ErrorPage.js";
 import Navbar from "./Components/Navbar/Navbar.js";
+import Footer from "./Components/Footer/Footer.js";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-    <Router>
+      <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact/>} />
-        <Route path="*" element={<ErrorPage/>} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
-    </Router>
+
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './Footer.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+  faYoutube,
+  faWhatsapp,
+} from '@fortawesome/free-brands-svg-icons';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-line">
+        <ul>
+            <p>Support</p>
+            <p>Company</p>
+            <p>Refund policy</p>
+            <p>Privacy Policy</p>
+            <p>Return Policy</p>
+            <p>Fashion Awards</p>
+            <p>Delivery Policy</p>
+            <p>Contact Us</p>
+            <p>Careers</p>
+        </ul>
+      </div>
+      <div className="footers-line">
+      <ul>
+            <p>Sitemap</p>
+            <p>Store Locator</p>
+            <p>Investor Relations</p>
+            <p>Blog</p>
+        </ul>
+      </div>
+      <div className="social-icons">
+        <FontAwesomeIcon icon={faFacebook} className='icon'/>
+        <FontAwesomeIcon icon={faTwitter} className='icon'/>
+        <FontAwesomeIcon icon={faInstagram} className='icon'/>
+        <FontAwesomeIcon icon={faYoutube} className='icon'/>
+        <FontAwesomeIcon icon={faWhatsapp} className='icon'/>
+      </div>
+      <div className="footer-line contact-info">
+        <p>Email Us- customercare@tcnsclothing.com | Call Us at +91-7583896939 | WhatsApp at +91-8448198618 | Timings: 10 AM to 6 PM, Mon to Sun. We are closed on bank holidays. </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
